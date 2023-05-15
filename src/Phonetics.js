b14176f1b4e5ae4e23c41bc05f1bbbbe1659ea86
a-blaine <ai.blaine4@protonmail.com>
2023-05-15 17:14:09 +0100

    Added Phonetics component and font awesome

---
+++ src/Phonetics.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+
+export default function Phonetics({ phonetics }) {
+  return (
+    <div className="Phonetics">
+      <a href={phonetics.audio} rel="noreferrer noopener" target="_blank">
+        <FontAwesomeIcon icon={faVolumeHigh} />
+      </a>{" "}
+      <span>{phonetics.text}</span>
+    </div>
+  );
+}
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import "./SearchResults.css";
 
 export default function SearchResults(props) {
   if (props.results) {
     return (
       <div className="Results text-start">
-        <h2>
-          {props.results.word}{" "}
-          <span className="phonetic">{props.results.phonetic}</span>
-        </h2>
+        <h2>{props.results.word} </h2>
 
+        {props.results.phonetics.map((phonetic, index) => {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetic} />
+            </div>
+          );
+        })}
         <hr />
+
         {props.results.meanings.map((meaning, index) => {
           return (
             <div key={index}>
