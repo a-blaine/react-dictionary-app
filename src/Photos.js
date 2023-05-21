6618ee424f861a49bdd99fb93e947d2e686ea2ca
a-blaine <ai.blaine4@protonmail.com>
2023-05-21 20:28:46 +0100

    Added styling and hyperlinks to photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos({ photos, keyword }) {
   if (photos) {
@@ -7,12 +8,14 @@ export default function Photos({ photos, keyword }) {
         <div className="row">
           {photos.map((photo, index) => {
             return (
-              <div className="col-3" key={index}>
-                <img
-                  src={photo.src.landscape}
-                  alt={keyword}
-                  className="img-fluid"
-                />
+              <div className="col-4" key={index}>
+                <a href={photo.url} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt={keyword}
+                    className="img-fluid"
+                  />
+                </a>
               </div>
             );
           })}
